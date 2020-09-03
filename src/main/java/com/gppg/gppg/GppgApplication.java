852e034eeff729dac8516a98c50f64a525095641
husheng <husheng4307@163.com>
2020-09-03 16:34:29 +0800

    修改数据库、redis指向

--- src/main/java/com/gppg/gppg/GppgApplication.java
+++ src/main/java/com/gppg/gppg/GppgApplication.java
@@ -1,9 +1,14 @@
 package com.gppg.gppg;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({
+        "com.gppg.gppg.mapper",
+        "com.gppg.gppg.student.mapper"
+})
 public class GppgApplication {
 
     public static void main(String[] args) {
